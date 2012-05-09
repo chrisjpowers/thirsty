0d7d922539c515b7fd9fc8be0d6e4c48f021212b
Chris Powers <chrisjpowers@gmail.com>
2012-05-09 09:45:55 -0500

    Reorganized with feat

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  */
 
 var express = require('express')
-  , users = require('./routes/users')
+  , feat = require("feat")
   , app = module.exports = express.createServer();
 
 // Configuration
@@ -15,34 +15,17 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(feat.middleware());
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(feat.gui());
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler());
 });
-
-
-var checkForDrinks= function(req,res,next){
-}
-
-// Routes
-console.log(users.list);
-app.get('/', users.index);
-
-
-app.get('/recent', users.recent);
-
-app.get('/users', users.list);
-
-app.post('/users', users.checkForDrinks, users.create);
-
-app.get('/users/:name', users.details);
-
-app.get('/:name', users.show);
 
 app.listen(8334);
 console.log("Thirsty is at your service %d in %s mode", app.address().port, app.settings.env);
